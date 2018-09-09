219cbe374197b970a011f873b7aa728087521773
jairorudas <jairorudasp@gmail.com>
2018-09-09 17:39:53 -0300

    componente tab Content criado

---
+++ src/utils/tabContent/TabContent.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import Card from '../card/card'
+import If from '../if'
+
+export default class TabContent extends Component {
+    constructor(props){
+        super(props)
+        this.state = {visible: false, _id: props.id}
+    }
+
+    render(){
+        return (
+            <If show={this.state._id === this.props.active}>
+                <div className={this.props.col}>
+                    {this.props.children}
+                    <Card />
+                </div>
+            </If>
+        )
+    }
+} 
