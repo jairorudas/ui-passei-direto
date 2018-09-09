5f22dee1daffb76a9ba7484ee5219acd3c74d99a
jairorudas <jairorudasp@gmail.com>
2018-09-09 17:40:42 -0300

    component father configured

--- src/App.js
+++ src/App.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import data from './data.json'
+import  TabContent from './utils/tabContent/TabContent'
+import Tabs from './utils/tabs/Tabs'
+
 
 class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = { 
+            active: 'Buscar', 
+            dataServer: data, 
+            input: '', 
+            favoritos: []
+        }
+        this.tabActive = this.tabActive.bind(this);
+        this.searchString = this.searchString.bind(this)
+        this.changeString = this.changeString.bind(this)
+    }        
+
+    tabActive(id){
+        this.setState(state => ({
+            active: id
+        }))
+    }
+
+    changeString(e){
+        this.setState({...this.state, input: e.target.value })
+    }
+
+    searchString(e){
+        e.preventDefault();
+        const params = this.state.input
+        //const url = 'https://search-api.passeidireto.com/api/Search/GlobalSearch?Query=teste&ContentTypeIds%5B%5D=1&PageNumber=0&PageSize=20&Order=2​'
+        // axios.get('./data.json')
+        //   .then( data => console.log(data))
+        //   .catch(err => console.log(err)) 
+        console.log(params);
+
+    }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+     <div className="container">
+        <div className="row">
+            <Tabs titulos={["Buscar", "Favoritos"]} onClick={this.tabActive} col="column column-25" active={this.state.active}/>      
+        </div>
+        <div className="row">
+            <TabContent id="Buscar" col="column" active={this.state.active}>
+
+                <form className="column" onSubmit={this.searchString}>
+                    <label>Digite o material que deseja: </label>
+                    <div className="row">
+                        <input className="column column-67"
+                                type="text"
+                                onChange={this.changeString}
+                                name="buscar" 
+                                placeholder="Digite o material que deseja." />
+                        <input className="button column-25" type="submit" value="Buscar!"  />
+                    </div>
+                </form> 
+
+        </TabContent>
+        <TabContent id="Favoritos" col="column" active={this.state.active}/>  
+        </div>
+     </div>
+
     );
   }
 }
