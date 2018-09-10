1939bcc302f4fd4ba5951661077cde918691d125
jairorudas <jairorudasp@gmail.com>
2018-09-10 02:01:14 -0300

    placeholder changed

--- src/utils/form/Form.jsx
+++ src/utils/form/Form.jsx
@@ -3,15 +3,16 @@ import './Form.scss'
 
 export default (props) => {
   return (
-      <form className="column form-pd" onSubmit={props.searchString} >
-        <label>Digite o material que deseja: </label>
+      <form className="column form-pd" onSubmit={props.search} >
+        <label htmlFor="buscar">Digite o material que deseja: </label>
         <div className="row">
             <input className="column-67"
                     type="text"
                     onChange={props.changeString}
+                    value={props.value}
                     name="buscar" 
                     placeholder="Digite o material que deseja." />
-            <input className="button column-25" type="submit" value="Buscar!"  />
+            <button className="button column-25" typeof="submit" >Buscar!</button>
         </div>
     </form> 
   )
